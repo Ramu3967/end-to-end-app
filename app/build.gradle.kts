d56989a60686f01b4ce5926405b2bf6a6e54e0d5
mramu_1996 <mandaloju.ramu@gmail.com>
2024-05-11 15:23:29 -0400

    Skeleton for hilt is ready. Also added paginated animals model.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -61,8 +63,16 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
 
-    implementation ("com.google.code.gson:gson:2.10")
+    // network
+    implementation ("com.squareup.retrofit2:retrofit:${rootProject.extra["retrofitVersion"]}")
 
+    implementation ("com.google.code.gson:gson:${rootProject.extra["gsonVersion"]}")
+    implementation ("com.squareup.okhttp3:okhttp:${rootProject.extra["gsonVersion"]}")
+    implementation ("com.squareup.okhttp3:logging-interceptor:${rootProject.extra["okHttpVersion"]}")
+
+    // DI
+    implementation ("com.google.dagger:hilt-android:${rootProject.extra["hiltVersion"]}")
+    kapt ("com.google.dagger:hilt-android-compiler:${rootProject.extra["hiltVersion"]}")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
