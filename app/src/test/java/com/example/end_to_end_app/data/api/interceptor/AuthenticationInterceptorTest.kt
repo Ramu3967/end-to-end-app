e92da3ee3638f1e1d79129bb5a0e22abfcdfde7a
mramu_1996 <mandaloju.ramu@gmail.com>
2024-05-11 22:12:30 -0400

    auth test invalid token failed(timeout)

--- app/src/test/java/com/example/end_to_end_app/data/api/interceptor/AuthenticationInterceptorTest.kt
+++ app/src/test/java/com/example/end_to_end_app/data/api/interceptor/AuthenticationInterceptorTest.kt
@@ -2,6 +2,7 @@ package com.example.end_to_end_app.data.api.interceptor
 
 import com.example.end_to_end_app.data.api.ApiConstants
 import com.example.end_to_end_app.data.api.ApiParameters
+import com.example.end_to_end_app.data.api.utils.JsonReader
 import com.example.end_to_end_app.data.preferences.Preferences
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -19,6 +20,7 @@ import org.mockito.Mockito.mock
 import org.robolectric.RobolectricTestRunner
 import java.time.Instant
 import com.google.common.truth.Truth.*
+import org.mockito.ArgumentMatchers
 
 @RunWith(RobolectricTestRunner::class)
 class AuthenticationInterceptorTest{
@@ -31,6 +33,9 @@ class AuthenticationInterceptorTest{
     private val endpointSeparator = "/"
     private val animalsEndpointPath = endpointSeparator + ApiConstants.ANIMALS_ENDPOINT
     private val validToken = "validToken"
+
+    private val authEndpointPath = endpointSeparator + ApiConstants.AUTH_ENDPOINT
+    private val expiredToken = "expiredToken"
 
     @Before
     fun setup() {
@@ -72,10 +77,66 @@ class AuthenticationInterceptorTest{
         }
     }
 
+    @Test
+    fun authenticationInterceptor_expiredToken() {
+        // Given
+        Mockito.`when`(preferences.getToken()).thenReturn(expiredToken)
+        Mockito.`when`(preferences.getTokenExpirationTime()).thenReturn(
+            // minus = expired
+            Instant.now().minusSeconds(3600).epochSecond
+        )
+        mockWebServer.dispatcher = getDispatcherForExpiredToken()
+        // When
+        okHttpClient.newCall(
+            Request.Builder()
+                .url(mockWebServer.url(ApiConstants.ANIMALS_ENDPOINT))
+                .build())
+            .execute()
+        // Then
+        val tokenRequest = mockWebServer.takeRequest()
+        val animalsRequest = mockWebServer.takeRequest()
+        tokenRequest.run {
+            assertThat(method).isEqualTo("POST")
+            assertThat(path).isEqualTo(authEndpointPath)
+        }
+        val inOrder = Mockito.inOrder(preferences)
+        inOrder.verify(preferences).getToken()
+        inOrder.verify(preferences).putToken(validToken)
+        Mockito.verify(preferences, Mockito.times(1)).getToken() // 5
+        Mockito.verify(preferences, Mockito.times(1)).putToken(validToken)
+        Mockito.verify(preferences, Mockito.times(1)).getTokenExpirationTime()
+        Mockito.verify(preferences, Mockito.times(1))
+            .putTokenExpirationTime(ArgumentMatchers.anyLong())
+        Mockito.verify(preferences, Mockito.times(1))
+            .putTokenType(ApiParameters.TOKEN_TYPE.trim())
+        Mockito.verifyNoMoreInteractions(preferences)
+        with(animalsRequest) { // 6
+            assertThat(method).isEqualTo("GET")
+            assertThat(path).isEqualTo(animalsEndpointPath)
+            assertThat(getHeader(ApiParameters.AUTH_HEADER))
+                .isEqualTo(ApiParameters.TOKEN_TYPE + validToken)
+        }
+    }
+
     private fun getDispatcherForValidToken() = object : Dispatcher() {
         override fun dispatch(request: RecordedRequest): MockResponse {
             return when (request.path) {
                 animalsEndpointPath -> MockResponse().setResponseCode(200)
+                else -> MockResponse().setResponseCode(404)
+            }
+        }
+    }
+
+    private fun getDispatcherForExpiredToken() = object : Dispatcher() {
+        override fun dispatch(request: RecordedRequest): MockResponse {
+            return when (request.path) {
+                authEndpointPath -> MockResponse()
+                    .setResponseCode(200)
+                    .setBody(JsonReader.getJson("networkresponses/validToken.json"))
+
+                animalsEndpointPath -> MockResponse()
+                    .setResponseCode(200)
+
                 else -> MockResponse().setResponseCode(404)
             }
         }
