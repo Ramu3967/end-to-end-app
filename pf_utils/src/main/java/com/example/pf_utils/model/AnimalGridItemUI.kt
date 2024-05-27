aaa56fc20c0d6aa7d50adc5fac59ac5ac403559a
mramu_1996 <mandaloju.ramu@gmail.com>
2024-05-27 15:13:51 -0400

    adjusted card view in the xml module.

--- pf_utils/src/main/java/com/example/pf_utils/model/AnimalGridItemUI.kt
+++ pf_utils/src/main/java/com/example/pf_utils/model/AnimalGridItemUI.kt
@@ -1,13 +1,19 @@
 package com.example.pf_utils.model
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -17,13 +23,33 @@ import com.example.pf_utils.R
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun AnimalUIElement(imgPath: String, desc: String) {
-    Column(modifier = Modifier.padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            GlideImage(
+                model = imgPath,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop,
 
-        GlideImage(model = imgPath, contentDescription = "", modifier=Modifier.size(150.dp)){
-            it.error(R.drawable.dog_placeholder)
+            ){ it.error(R.drawable.dog_placeholder)}
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = desc,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
         }
-
-        Text(text= desc, style = TextStyle(textAlign = TextAlign.Center))
-
     }
 }
+
